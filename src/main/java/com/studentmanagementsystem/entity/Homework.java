142b40b2415ff804f8852b583c80c2845e5f9ce6
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-25 16:38:32 +0530

    Homework Entity

---
+++ src/main/java/com/studentmanagementsystem/entity/Homework.java
@@ -0,0 +1,34 @@
+package com.studentmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "homework")
+public class Homework {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String content;
+
+    @Column(name = "grade", columnDefinition = "varchar(2) default 'NA'")
+    private String gradeObtained;
+
+    private String details;
+
+    @Column(name = "date")
+    private Date dateCreated;
+
+    @ManyToOne
+    @ToString.Exclude
+    private Student student;
+}
