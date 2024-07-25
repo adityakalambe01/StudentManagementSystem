10b4289abdffc67021f4ccc8a3492c170d0d2745
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-25 16:43:06 +0530

    Parent Repository

---
+++ src/main/java/com/studentmanagementsystem/repo/ParentRepository.java
@@ -0,0 +1,9 @@
+package com.studentmanagementsystem.repo;
+
+import com.studentmanagementsystem.entity.Parent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParentRepository extends JpaRepository<Parent, Integer> {
+}
