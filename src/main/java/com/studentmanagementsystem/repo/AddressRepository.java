fd50a6ac65909cb4edf221aadc35d5407304f674
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-27 16:29:20 +0530

    Address Repository Update

--- src/main/java/com/studentmanagementsystem/repo/AddressRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/AddressRepository.java
@@ -1,10 +1,31 @@
 package com.studentmanagementsystem.repo;
 
 import com.studentmanagementsystem.entity.Address;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
+    Address save(Address address);
+
+    Page<Address> findAll(Pageable pageable);
+
+    Address findById(int id);
+
+    List<Address> findByAreaContaining(String area);
+
+    List<Address> findByCityContaining(String city);
+
+    List<Address> findByStateContaining(String state);
+
+    List<Address> findByDistrictContaining(String district);
+
+    List<Address> findByZip(Integer zipcode);
+
+    Address deleteById(int id);
 
 }
