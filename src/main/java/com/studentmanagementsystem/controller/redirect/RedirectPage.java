b9cc0cb3aec4d870034233b00e933dc6176b2876
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-27 16:38:17 +0530

    Redirect

---
+++ src/main/java/com/studentmanagementsystem/controller/redirect/RedirectPage.java
@@ -0,0 +1,16 @@
+package com.studentmanagementsystem.controller.redirect;
+
+import com.studentmanagementsystem.service.AddressService;
+import com.studentmanagementsystem.service.ClassroomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RedirectPage {
+    @Autowired
+    private AddressService addressService;
+
+
+
+
+}
