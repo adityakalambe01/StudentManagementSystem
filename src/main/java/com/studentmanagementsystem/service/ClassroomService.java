0c22baca1a693497923f4e750a111ddfc6ad9279
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-27 16:30:19 +0530

    Classroom Service Impl Update

--- src/main/java/com/studentmanagementsystem/service/ClassroomService.java
+++ src/main/java/com/studentmanagementsystem/service/ClassroomService.java
@@ -1,4 +1,23 @@
 package com.studentmanagementsystem.service;
 
+import com.studentmanagementsystem.entity.Classroom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ClassroomService {
+    Classroom save(Classroom classroom);
+
+    Classroom findById(int id);
+
+    Classroom findByName(String name);
+
+    Classroom findByCode(String code);
+
+    Page<Classroom> findAll(Pageable pageable);
+
+    Classroom deleteById(int id);
+
+    Classroom deleteByCode(String code);
+
+    Classroom deleteByName(String name);
 }
