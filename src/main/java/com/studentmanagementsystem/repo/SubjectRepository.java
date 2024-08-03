c705d8bb1675c075aff65a02da057d78a96a904c
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:22:29 +0530

    Subject Repository Update

--- src/main/java/com/studentmanagementsystem/repo/SubjectRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/SubjectRepository.java
@@ -1,9 +1,21 @@
 package com.studentmanagementsystem.repo;
 
+import com.studentmanagementsystem.entity.Student;
 import com.studentmanagementsystem.entity.Subject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+    Student findById(int id);
+
+    List<Subject> findByNameContaining(String name);
+
+    Page<Subject> findAll(Pageable pageable);
+
+    Page<Student> findAllByName(Pageable pageable, String name);
 }
