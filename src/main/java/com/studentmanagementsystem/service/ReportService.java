58c90eb0745462cd326e254fcba671fd4f1d7afe
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:28:50 +0530

    Report Service Update

--- src/main/java/com/studentmanagementsystem/service/ReportService.java
+++ src/main/java/com/studentmanagementsystem/service/ReportService.java
@@ -1,4 +1,25 @@
 package com.studentmanagementsystem.service;
 
+import com.studentmanagementsystem.entity.Report;
+import com.studentmanagementsystem.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Date;
+import java.util.List;
+
 public interface ReportService {
+    Page<Report> findAll(Pageable pageable);
+
+    Report findById(int id);
+
+    Report deleteById(int id);
+
+    List<Report> findByDateCreated(Date created);
+
+    List<Report> findByContentContaining(String content);
+
+    List<Report> findByTeacherCommentContaining(String comment);
+
+    List<Report> findByStudent(Student student);
 }
