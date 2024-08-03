facccb89862c627e2531d86d1df217171d1e8174
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:29:21 +0530

    Subject Service Update

--- src/main/java/com/studentmanagementsystem/service/SubjectService.java
+++ src/main/java/com/studentmanagementsystem/service/SubjectService.java
@@ -1,4 +1,18 @@
 package com.studentmanagementsystem.service;
 
+import com.studentmanagementsystem.entity.Student;
+import com.studentmanagementsystem.entity.Subject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface SubjectService {
+    Student findById(int id);
+
+    List<Subject> findByNameContaining(String name);
+
+    Page<Subject> findAll(Pageable pageable);
+
+    Page<Student> findAllByName(Pageable pageable, String name);
 }
