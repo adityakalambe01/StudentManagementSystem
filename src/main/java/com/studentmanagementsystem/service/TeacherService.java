56891ac0d0ce6b4abf15cef4092a22debcf36f05
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:29:37 +0530

    Teacher Service Update

--- src/main/java/com/studentmanagementsystem/service/TeacherService.java
+++ src/main/java/com/studentmanagementsystem/service/TeacherService.java
@@ -1,4 +1,29 @@
 package com.studentmanagementsystem.service;
 
+import com.studentmanagementsystem.entity.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface TeacherService {
+    Teacher findById(int id);
+
+    List<Teacher> findByFirstNameContaining(String firstName);
+
+    List<Teacher> findByMiddleNameContaining(String middleName);
+
+    List<Teacher> findByLastNameContaining(String lastName);
+
+    List<Teacher> findByEmailContaining(String email);
+
+    List<Teacher> findByPhoneNumberContaining(String phoneNumber);
+
+    List<Teacher> findByGenderContaining(String gender);
+
+    List<Teacher> findByFirstNameOrMiddleNameOrLastNameOrEmailContaining(String firstName, String middleName, String lastName, String email);
+
+    Page<Teacher> findAll(Pageable pageable);
+
+    Teacher deleteById(int id);
 }
