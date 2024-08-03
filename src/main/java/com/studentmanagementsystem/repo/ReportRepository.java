f6c72e4a43df2789fad93bc3c5499ed9fab818c8
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:21:55 +0530

    Report Repository Update

--- src/main/java/com/studentmanagementsystem/repo/ReportRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/ReportRepository.java
@@ -1,9 +1,28 @@
 package com.studentmanagementsystem.repo;
 
 import com.studentmanagementsystem.entity.Report;
+import com.studentmanagementsystem.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
+    Page<Report> findAll(Pageable pageable);
+
+    Report findById(int id);
+
+    Report deleteById(int id);
+
+    List<Report> findByDateCreated(Date created);
+
+    List<Report> findByContentContaining(String content);
+
+    List<Report> findByTeacherCommentContaining(String comment);
+
+    List<Report> findByStudent(Student student);
 }
