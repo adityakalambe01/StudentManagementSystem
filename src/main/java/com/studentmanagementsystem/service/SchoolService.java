01fdbda801c7aa0f4aa28add4585f5da4b504afe
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:29:00 +0530

    School Service Update

--- src/main/java/com/studentmanagementsystem/service/SchoolService.java
+++ src/main/java/com/studentmanagementsystem/service/SchoolService.java
@@ -1,4 +1,27 @@
 package com.studentmanagementsystem.service;
 
+import com.studentmanagementsystem.entity.School;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public interface SchoolService {
+    School findById(int id);
+
+    School save(School school);
+
+    School updateSchoolAddress(@PathVariable("schoolId") int schoolId, @PathVariable("addressId") int addressId);
+
+    School deleteById(int id);
+
+    School findByName(String name);
+
+    List<School> findByNameContaining(String name);
+
+    List<School> findAll();
+
+    List<School> findByPrincipleContaining(String principle);
+
+    List<School> findByDetailsContaining(String details);
+
 }
