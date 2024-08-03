f1609f9e3f3c7684d89748aba3a7d6229d4f1464
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:22:04 +0530

    School Repository Update

--- src/main/java/com/studentmanagementsystem/repo/SchoolRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/SchoolRepository.java
@@ -4,6 +4,24 @@ import com.studentmanagementsystem.entity.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SchoolRepository extends JpaRepository<School, Integer> {
+    School findById(int id);
+
+    School save(School school);
+
+    School deleteById(int id);
+
+    School findByName(String name);
+
+    List<School> findByNameContaining(String name);
+
+    List<School> findAll();
+
+    List<School> findByPrincipleContaining(String principle);
+
+    List<School> findByDetailsContaining(String details);
+
 }
