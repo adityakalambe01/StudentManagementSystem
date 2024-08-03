fcadbca25fd5d9ed16e1e5804a4549736032ceba
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:22:41 +0530

    Teacher Repository Update

--- src/main/java/com/studentmanagementsystem/repo/TeacherRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/TeacherRepository.java
@@ -1,9 +1,32 @@
 package com.studentmanagementsystem.repo;
 
 import com.studentmanagementsystem.entity.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+    Teacher findById(int id);
+
+    List<Teacher> findByFirstNameContaining(String firstName);
+
+    List<Teacher> findByMiddleNameContaining(String middleName);
+
+    List<Teacher> findByLastNameContaining(String lastName);
+
+    List<Teacher> findByEmailContaining(String email);
+
+    List<Teacher> findByPhoneNumberContaining(String phoneNumber);
+
+    List<Teacher> findByGenderContaining(String gender);
+
+    List<Teacher> findByFirstNameOrMiddleNameOrLastNameOrEmailContaining(String firstName, String middleName, String lastName, String email);
+
+    Page<Teacher> findAll(Pageable pageable);
+
+    Teacher deleteById(int id);
 }
