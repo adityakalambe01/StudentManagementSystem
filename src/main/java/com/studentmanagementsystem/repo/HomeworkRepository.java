15631b5dae3152866050e206a4ac254a3dc89a36
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:21:26 +0530

    Homework Repository Update

--- src/main/java/com/studentmanagementsystem/repo/HomeworkRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/HomeworkRepository.java
@@ -1,9 +1,32 @@
 package com.studentmanagementsystem.repo;
 
+import com.studentmanagementsystem.entity.Classroom;
 import com.studentmanagementsystem.entity.Homework;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface HomeworkRepository extends JpaRepository<Homework, Integer> {
+    Classroom save(Classroom classroom);
+
+    Classroom findById(int id);
+
+    Page<Homework> findAll(Pageable pageable);
+
+    List<Homework> findByDateCreated(Date date);
+
+    List<Homework> findByDetailsContaining(String details);
+
+    List<Homework> findByGradeObtained(String grade);
+
+    List<Homework> findByGradeObtainedIsIn(List<String> str);
+
+    List<Homework> findByContentContaining(String content);
+
+
 }
