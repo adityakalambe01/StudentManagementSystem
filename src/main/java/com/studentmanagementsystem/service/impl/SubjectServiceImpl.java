7443ec243f040e3d6c0d73345edee6a1e9c3edaf
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 01:30:42 +0530

    Subject Service Implementation Update

---
+++ src/main/java/com/studentmanagementsystem/service/impl/SubjectServiceImpl.java
@@ -0,0 +1,40 @@
+package com.studentmanagementsystem.service.impl;
+
+import com.studentmanagementsystem.entity.Student;
+import com.studentmanagementsystem.entity.Subject;
+import com.studentmanagementsystem.repo.SubjectRepository;
+import com.studentmanagementsystem.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class SubjectServiceImpl implements SubjectService {
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Override
+    public Student findById(int id) {
+        return subjectRepository.findById(id);
+    }
+
+    @Override
+    public List<Subject> findByNameContaining(String name) {
+        return subjectRepository.findByNameContaining(name);
+    }
+
+    @Override
+    public Page<Subject> findAll(Pageable pageable) {
+        return subjectRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Student> findAllByName(Pageable pageable, String name) {
+        return subjectRepository.findAllByName(pageable, name);
+    }
+}
