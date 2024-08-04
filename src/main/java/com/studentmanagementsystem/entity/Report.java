6507e939ac6fae08d2b0d104e9ba6d7902a4711c
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 19:47:09 +0530

    Report Entity Update

--- src/main/java/com/studentmanagementsystem/entity/Report.java
+++ src/main/java/com/studentmanagementsystem/entity/Report.java
@@ -1,5 +1,6 @@
 package com.studentmanagementsystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,6 +27,7 @@ public class Report {
     private String teacherComment;
 
     @ManyToOne
+    @JsonIgnore
     @ToString.Exclude
     private Student student;
 }
