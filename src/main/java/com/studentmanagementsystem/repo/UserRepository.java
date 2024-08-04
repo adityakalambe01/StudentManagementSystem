0029d9afe9605b182762a379c081c494e9acb256
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-05 00:24:05 +0530

    User Repository Update

--- src/main/java/com/studentmanagementsystem/repo/UserRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/UserRepository.java
@@ -1,6 +1,7 @@
 package com.studentmanagementsystem.repo;
 
 import com.studentmanagementsystem.entity.Users;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@NonNullApi
 public interface UserRepository extends JpaRepository<Users, String> {
 
     Users save(Users users);
@@ -18,12 +20,9 @@ public interface UserRepository extends JpaRepository<Users, String> {
 
     Users findByEmailId(String email);
 
-//    @Query(value = "select * from Users where Users.emailId='%?1%'", nativeQuery = true)
-//    Users findByEmailIdContaining(String emailId, Pageable pageable);
-
     Page<Users> findAll(Pageable pageable);
 
-    @Query(value = "select * from Users where Users.role=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.role=?1", nativeQuery = true)
     List<Users> findByRole(String role, Pageable pageable);
 
     List<Users> findByFirstNameContaining(String firstName, Pageable pageable);
@@ -34,24 +33,24 @@ public interface UserRepository extends JpaRepository<Users, String> {
 
     List<Users> findByFirstNameContainingAndMiddleNameContainingAndLastNameContaining(String firstName, String middleName, String lastName, Pageable pageable);
 
-    @Query(value = "select * from Users where Users.phoneNumber=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.phoneNumber=?1", nativeQuery = true)
     List<Users> findByPhoneNumberContaining(String phoneNumber, Pageable pageable);
 
-    @Query(value = "select * from Users where Users.isSuperAdminAccess=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.isSuperAdminAccess=?1", nativeQuery = true)
     List<Users> findByIdSuperAdminAccess(boolean isSuperAdmin, Pageable pageable);
 
-    @Query(value = "select * from Users where Users.isAdminAccess=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.isAdminAccess=?1", nativeQuery = true)
     List<Users> findByIsAdminAccess(boolean isAdmin, Pageable pageable);
 
-    @Query(value = "select * from Users where Users.isPrincipleAccess=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.isPrincipleAccess=?1", nativeQuery = true)
     List<Users> findByIsPrincipleAccess(boolean isPrinciple, Pageable pageable);
 
-    @Query(value = "select * from Users where Users.isTeacherAccess=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.isTeacherAccess=?1", nativeQuery = true)
     List<Users> findByIsTeacherAccess(boolean isTeacher, Pageable pageable);
 
-    @Query(value = "select * from Users where Users.isStudentAccess=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.isStudentAccess=?1", nativeQuery = true)
     List<Users> findByIsStudentAccess(boolean isStudent, Pageable pageable);
 
-    @Query(value = "select * from Users where Users.isParentAccess=1", nativeQuery = true)
+    @Query(value = "select * from Users where Users.isParentAccess=?1", nativeQuery = true)
     List<Users> findByIsParentAccess(boolean isParent, Pageable pageable);
 }
