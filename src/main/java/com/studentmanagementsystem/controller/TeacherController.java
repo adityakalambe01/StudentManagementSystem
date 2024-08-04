b0f60e45fa3dd9944141ca9270f1f088676a13ce
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 10:15:12 +0530

    Teacher Controller Update

--- src/main/java/com/studentmanagementsystem/controller/TeacherController.java
+++ src/main/java/com/studentmanagementsystem/controller/TeacherController.java
@@ -4,8 +4,11 @@ import com.studentmanagementsystem.entity.Teacher;
 import com.studentmanagementsystem.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -48,8 +51,12 @@ public class TeacherController {
         return teacherService.findByFirstNameOrMiddleNameOrLastNameOrEmailContaining(firstName, middleName, lastName, email);
     }
 
-    public Page<Teacher> findAll(Pageable pageable){
-        return teacherService.findAll(pageable);
+    @GetMapping
+    public List<Teacher> findAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer pageNumber,
+            @RequestParam(value = "size", defaultValue = "50", required = false) Integer pageSize
+    ){
+        return teacherService.findAll(PageRequest.of(pageNumber,pageSize));
     }
 
     public Teacher deleteById(int id){
