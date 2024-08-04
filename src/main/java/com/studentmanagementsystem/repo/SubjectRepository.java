8a7a40588d93f14d1f6ecdb2bd0605b77f6d6bc9
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-05 00:23:47 +0530

    Subject Repository Update

--- src/main/java/com/studentmanagementsystem/repo/SubjectRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/SubjectRepository.java
@@ -2,6 +2,7 @@ package com.studentmanagementsystem.repo;
 
 import com.studentmanagementsystem.entity.Student;
 import com.studentmanagementsystem.entity.Subject;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@NonNullApi
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
     Student findById(int id);
 
