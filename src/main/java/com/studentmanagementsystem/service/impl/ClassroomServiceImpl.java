61e255a5cd56e7939a6f5b10470ae91932be22ca
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-04 19:49:08 +0530

    Classroom Service Implementation Update

--- src/main/java/com/studentmanagementsystem/service/impl/ClassroomServiceImpl.java
+++ src/main/java/com/studentmanagementsystem/service/impl/ClassroomServiceImpl.java
@@ -3,11 +3,14 @@ package com.studentmanagementsystem.service.impl;
 import com.studentmanagementsystem.entity.Classroom;
 import com.studentmanagementsystem.repo.ClassroomRepository;
 import com.studentmanagementsystem.service.ClassroomService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
 
 @Service
 @Transactional
@@ -53,5 +56,20 @@ public class ClassroomServiceImpl implements ClassroomService {
     @Override
     public Classroom deleteByName(String name) {
         return classroomRepository.deleteByName(name);
+    }
+
+    @PostConstruct
+    public void init() {
+        if (classroomRepository.count() == 0) {
+            for (int i = 1; i <= 100; i++) {
+                Classroom classroom = new Classroom();
+
+                classroom.setCode(new RandomNameGenerator().next(26, 3));
+
+                classroom.setName(new RandomNameGenerator().next(26, 5));
+
+                classroomRepository.save(classroom);
+            }
+        }
     }
 }
