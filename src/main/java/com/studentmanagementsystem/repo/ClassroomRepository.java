9bbb6fc5e2d9a063dd56079cdbe23047d7000c90
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-05 00:22:51 +0530

    Class Room Repository Update

--- src/main/java/com/studentmanagementsystem/repo/ClassroomRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/ClassroomRepository.java
@@ -1,12 +1,14 @@
 package com.studentmanagementsystem.repo;
 
 import com.studentmanagementsystem.entity.Classroom;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@NonNullApi
 public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
     Classroom save(Classroom classroom);
 
