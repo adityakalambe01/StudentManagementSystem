19d6dc6de92e922eeebed354731b74e53c307044
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-05 00:23:38 +0530

    Student Repository Update

--- src/main/java/com/studentmanagementsystem/repo/StudentRepository.java
+++ src/main/java/com/studentmanagementsystem/repo/StudentRepository.java
@@ -1,7 +1,7 @@
 package com.studentmanagementsystem.repo;
 
-import com.studentmanagementsystem.entity.School;
 import com.studentmanagementsystem.entity.Student;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@NonNullApi
 public interface StudentRepository extends JpaRepository<Student, Integer> {
     Student findById(int id);
 
