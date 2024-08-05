f3d2a5ab627c9894ac02bf68d976fb55bb82ed50
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-05 17:06:24 +0530

    Homework Service Implementation sUpdate

--- src/main/java/com/studentmanagementsystem/service/impl/HomeworkServiceImpl.java
+++ src/main/java/com/studentmanagementsystem/service/impl/HomeworkServiceImpl.java
@@ -1,10 +1,8 @@
 package com.studentmanagementsystem.service.impl;
 
-import com.studentmanagementsystem.entity.Classroom;
 import com.studentmanagementsystem.entity.Homework;
 import com.studentmanagementsystem.repo.HomeworkRepository;
 import com.studentmanagementsystem.service.HomeworkService;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 @Service
 @Transactional
@@ -21,52 +18,112 @@ public class HomeworkServiceImpl implements HomeworkService {
     @Autowired
     private HomeworkRepository homeworkRepository;
 
+    /*
+    *
+    * Add Homework
+    *
+    * */
     @Override
-    public Classroom save(Classroom classroom) {
-        return homeworkRepository.save(classroom);
+    public Homework save(Homework homework) {
+        return homeworkRepository.save(homework);
     }
 
+    /*
+    *
+    * Update Homework
+    *
+    * */
     @Override
-    public Classroom findById(int id) {
+    public Homework update(int homeworkId, Homework homework) {
+
+        return null;
+    }
+
+    /*
+    *
+    * Homework by I'd
+    *
+    * */
+    @Override
+    public Homework findById(int id) {
         return homeworkRepository.findById(id);
     }
 
+    /*
+    *
+    * Homework List by Pagination
+    *
+    * */
     @Override
     public Page<Homework> findAll(Pageable pageable) {
         return homeworkRepository.findAll(pageable);
     }
 
+    /*
+    *
+    * Homework List by Date
+    *
+    * */
     @Override
     public List<Homework> findByDateCreated(Date date) {
         return homeworkRepository.findByDateCreated(date);
     }
 
+    /*
+    *
+    * Homework List by Homework Details
+    *
+    * */
     @Override
     public List<Homework> findByDetailsContaining(String details) {
         return homeworkRepository.findByDetailsContaining(details);
     }
 
+    /*
+    *
+    * Homework List by Grade Obtained
+    *
+    * */
     @Override
     public List<Homework> findByGradeObtained(String grade) {
         return homeworkRepository.findByGradeObtained(grade);
     }
 
+    /*
+    *
+    * Homework List by Grade Obtained List
+    *
+    * */
     @Override
     public List<Homework> findByGradeObtainedIsIn(List<String> str) {
         return homeworkRepository.findByGradeObtainedIsIn(str);
     }
 
+    /*
+    *
+    * Homework List by Content
+    *
+    * */
     @Override
     public List<Homework> findByContentContaining(String content) {
         return homeworkRepository.findByContentContaining(content);
     }
 
+    /*
+    *
+    * Save all Homework
+    *
+    * */
     @Override
     public List<Homework> saveAll(List<Homework> list) {
         return homeworkRepository.saveAll(list);
     }
 
-    @PostConstruct
+    /*
+    *
+    * Add Dummy Data
+    *
+    * */
     public void init() {
         if (homeworkRepository.count() == 0) {
             for (int i = 1; i <= 250; i++) {
