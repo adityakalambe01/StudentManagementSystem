563c3961abe34fd070ba264f26d7addcd118136d
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-05 17:13:54 +0530

    Users Controller Update

--- src/main/java/com/studentmanagementsystem/controller/UserController.java
+++ src/main/java/com/studentmanagementsystem/controller/UserController.java
@@ -6,11 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "api/users")
