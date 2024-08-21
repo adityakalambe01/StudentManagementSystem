e26af4cf37a1acb310b8bed7d24e158002156ac4
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-21 17:13:41 +0530

    Parent Controller Update

--- src/main/java/com/studentmanagementsystem/controller/ParentController.java
+++ src/main/java/com/studentmanagementsystem/controller/ParentController.java
@@ -18,41 +18,81 @@ public class ParentController {
     @Autowired
     private ParentService parentService;
 
+    /*
+    *
+    * Get All Parent's
+    *
+    * */
     @GetMapping
     public List<Parent> findAll(Pageable pageable){
         return parentService.findAll(pageable);
     }
 
+    /*
+    *
+    * Get Parent's By ID
+    *
+    * */
     @GetMapping(value = "/{id}")
     public Parent findById(@PathVariable("id") int id){
         return parentService.findById(id);
     }
 
+    /*
+    *
+    * Get Parent's by First-Name
+    *
+    * */
     @GetMapping(value = "/first-name-{firstName}")
     public List<Parent> findByFirstNameContaining(@PathVariable("firstName") String name){
         return parentService.findByFirstNameContaining(name);
     }
 
+    /*
+    *
+    * Get Parent's by Middle-Name
+    *
+    * */
     @GetMapping(value = "/middle-name-{middleName}")
     public List<Parent> findByMiddleNameContaining(@PathVariable("middleName") String name){
         return parentService.findByMiddleNameContaining(name);
     }
 
+    /*
+    *
+    * Get Parent's by Last-Name
+    *
+    * */
     @GetMapping(value = "/last-name-{lastName}")
     public List<Parent> findByLastNameContaining(@PathVariable("lastName") String name){
         return parentService.findByLastNameContaining(name);
     }
 
+    /*
+    *
+    * Get Parent's by First-Name and Last-Name
+    *
+    * */
     @GetMapping(value = "/fist-name-{firstName}-last-name-{lastName}")
     public List<Parent> findByFirstNameAndMiddleNameContaining(@PathVariable("firstName") String firstName,@PathVariable("lastName") String middleName){
         return parentService.findByFirstNameAndMiddleNameContaining(firstName, middleName);
     }
 
+    /*
+    *
+    * Get Parent's by First-Name and Last-Name
+    *
+    * */
     @GetMapping(value = "/last-name-{lastNAme}-first-name-{firstName}")
     public List<Parent> findByLastNameAndFirstNameContaining(@PathVariable("lastNAme") String lastName,@PathVariable("firstName") String firstName){
         return parentService.findByLastNameAndFirstNameContaining(lastName, firstName);
     }
 
+    /*
+    *
+    * Get Parent's by First-Name and Last-Name
+    *
+    * */
     @GetMapping(value = "/first-name-{firstName}-middle-name-{middleName}-last-name-{lastName}")
     public List<Parent> findByFirstNameAndMiddleNameAndLastNameContaining(@PathVariable("firstName") String firstName,@PathVariable("middleName") String middleName,@PathVariable("lastName") String lastName){
         return parentService.findByFirstNameAndMiddleNameAndLastNameContaining(firstName, middleName, lastName);
