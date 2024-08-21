5f6fdd93e6b036be918054c44eea1602de34d7cf
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-21 19:14:17 +0530

    User Service Implementation Update

--- src/main/java/com/studentmanagementsystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/studentmanagementsystem/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.studentmanagementsystem.entity.Users;
 import com.studentmanagementsystem.repo.UserRepository;
 import com.studentmanagementsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
