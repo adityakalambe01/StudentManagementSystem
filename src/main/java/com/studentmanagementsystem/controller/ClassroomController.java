892fc5e6e9accb5076eeed636a55fa622baa605c
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-21 17:13:23 +0530

    Class Room Controller Update

--- src/main/java/com/studentmanagementsystem/controller/ClassroomController.java
+++ src/main/java/com/studentmanagementsystem/controller/ClassroomController.java
@@ -14,16 +14,31 @@ public class ClassroomController {
     @Autowired
     private ClassroomService classroomService;
 
+    /*
+    *
+    * Add New Class Room
+    *
+    * */
     @PostMapping
     public Classroom save(Classroom classroom) {
         return classroomService.save(classroom);
     }
 
+    /*
+    *
+    * Get Class Room By ID
+    *
+    * */
     @GetMapping("/{id}")
     public Classroom findById(@PathVariable("id") int id) {
         return classroomService.findById(id);
     }
 
+    /*
+    *
+    * Get All Class Room's
+    *
+    * */
     @GetMapping
     public List<Classroom> findAll(
             @RequestParam(value = "page", defaultValue = "0", required = false) Integer pageNumber,
@@ -32,26 +47,51 @@ public class ClassroomController {
         return classroomService.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
     }
 
+    /*
+    *
+    * Get Class Room
+    *
+    * */
     @GetMapping("/name-{name}")
     public Classroom findByName(@PathVariable("name") String name) {
         return classroomService.findByName(name);
     }
 
+    /*
+    *
+    * Get Class Room By Code
+    *
+    * */
     @GetMapping("/code-{code}")
     public Classroom findByCode(@PathVariable("code") String code) {
         return classroomService.findByCode(code);
     }
 
+    /*
+    *
+    * Delete Class Room By ID
+    *
+    * */
     @DeleteMapping("/{id}")
     public Classroom deleteById(@PathVariable("id") int id) {
         return classroomService.deleteById(id);
     }
 
+    /*
+    *
+    * Delete Class Room By Code
+    *
+    * */
     @DeleteMapping("/code-{code}")
     public Classroom deleteByCode(@PathVariable("code") String code) {
         return classroomService.deleteByCode(code);
     }
 
+    /*
+    *
+    * Delete Class Room By Name
+    *
+    * */
     @DeleteMapping("/name-{name}")
     public Classroom deleteByName(@PathVariable("name") String name) {
         return classroomService.deleteByName(name);
