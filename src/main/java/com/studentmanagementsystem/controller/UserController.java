2ce12935de29e250356c42f8020a0ea4dd1a3478
Aditya Kalambe <adityakalambe20@gmail.com>
2024-08-21 19:10:51 +0530

    User Controller Update

--- src/main/java/com/studentmanagementsystem/controller/UserController.java
+++ src/main/java/com/studentmanagementsystem/controller/UserController.java
@@ -4,7 +4,6 @@ import com.studentmanagementsystem.entity.Users;
 import com.studentmanagementsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
